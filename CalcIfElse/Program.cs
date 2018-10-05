35d6d5536116ccd90070908bf181a900ba379595
ARTICUS939 <articus_mib@mail.ru>
2018-10-05 17:39:13 +0300

    калькулятор if else

--- CalcIfElse/Program.cs
+++ CalcIfElse/Program.cs
@@ -10,6 +10,46 @@ namespace CalcIfElse
     {
         static void Main(string[] args)
         {
+            double a;
+            double b;
+            double total;
+            char oper;
+
+            Console.WriteLine("Введите первое число:");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите действие:");
+            oper = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число:");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            if (oper == '+')
+            {
+                total = a + b;
+                Console.WriteLine("Ответ: " + total);
+            }
+
+            else if (oper == '-')
+            {
+                total = a - b;
+                Console.WriteLine("Ответ: " + total);
+            }
+
+            else if (oper == '*')
+            {
+                total = a * b;
+                Console.WriteLine("Ответ: " + total);
+            }
+
+            else if (oper == '/')
+            {
+                total = a / b;
+                Console.WriteLine("Ответ: " + total);
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
